79f11d8927b6ac2752ada799d3a637dbe71e2aaa
Alex Ford <alex.ford@codetunnel.com>
2015-05-13 10:07:51 -0600

    Added symlinks for classic vim and macvim.

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ git submodule init && git submodule update
 
 # Remove all dotfiles from the home directory if present.
 echo Removing any existing dotfiles from your home directory.
-rm -rf ~/.nvim ~/.nvimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme ~/.tmux
+rm -rf ~/.vim ~/.vimrc ~/.nvim ~/.nvimrc ~/.bashrc ~/.bash_profile ~/.inputrc ~/.gitconfig ~/.shell_prompt.sh ~/.tmux.conf ~/.tmux_theme ~/.tmux
 
 # Initialize symlinks.
 echo Creating symlinks in your home directory that point to this dotfiles repository.
 ln -s "$PWD/.nvim" ~/.nvim
 ln -s "$PWD/.nvim/nvimrc" ~/.nvimrc
+ln -s "$PWD/.nvim" ~/.vim
+ln -s "$PWD/.nvim/nvimrc" ~/.vimrc
 ln -s "$PWD/.bashrc" ~/.bashrc
 ln -s "$PWD/.bash_profile" ~/.bash_profile
 ln -s "$PWD/.inputrc" ~/.inputrc
